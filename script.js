5f8eb84bb1c3689efb0c9aebff012955e90ef05a
Joseph Ononaye <josephononaye@gmail.com>
2023-09-27 23:52:36 +0100

    Created the model quiz question with three options

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
 // Start the timer and launch a question.
 
+// Pulling DOM
 var timer = document.querySelector(".timer");
-
+var options = document.querySelectorAll(".options");
 var questionTitle = document.querySelector("#question-title")
 
-var question1 = "HTML stands for:"
 
 
-function theTimer() {
-    var timeLeft = 10;
 
-    var timeInterval = setInterval(function() {
-if (timeLeft > -1) {
-timer.textContent = " Time left: " + timeLeft;
-timeLeft--;
-    }
-        },
-
-            1000);
+// Quizzes
+var question1 = {
+    title: "HTML stands for:",
+    options: ["Hypertext Makeup LAN", "Hightext Makeup Language", "Hypertext Markup Language"],
+    correctAns: 2
 }
 
-theTimer()
+
+// Question Display
+function showQuestion() {
+ questionTitle.textContent = question1.title
+
+}
+
+showQuestion();
+
+
+// Options Display
+options.forEach(function(element, index) {
+element.textContent = question1.options[index]
+});
+
+
+
+
+// function theTimer() {
+//     var timeLeft = 10;
+//     var timeInterval = setInterval(function() {
+// if (timeLeft > -1) {
+// timer.textContent = " Time left: " + timeLeft;
+// timeLeft--;
+ 
+//     }
+//         },
+
+//             1000);
+// }
+
+// theTimer()
+
+
+
+
 
 
 
