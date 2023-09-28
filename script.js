49afbd3cb48e004e0cf535679e8b0e508a276fd0
Joseph Ononaye <josephononaye@gmail.com>
2023-09-28 21:18:55 +0100

    I included a 'next button' to navigate to the other questions by modifying both the CSS and HTML while working on the Javascript

--- script.js
+++ script.js
@@ -2,84 +2,111 @@
 
 // Pulling DOM
 var timer = document.querySelector(".timer");
-var questionTitle = document.querySelector("#question-title")
+var questionTitle = document.querySelector("#question-title");
 var options = document.querySelectorAll(".options");
-
+var nextBtn = document.querySelector("#next");
+var score = 0;
+var timeLeft = 10;
 
 // Quizzes
 var question = [
     {
     title: "HTML stands for:",
-    options: ["Hypertext Makeup LAN", "Hightext Makeup Language", "Hypertext Markup Language"],
+    options: ["Hypertext Makeup LAN", "Hightext Makeup Language", "Hypertext Markup Language", "Hardtext Memory Language"],
     correctAns: 2
 },
 {
     title: "CSS stands for:",
-    options: ["Cascading Style Sheet", "Cascadind Support Signals", "Custom Style Sheet"],
+    options: ["Custom style Sheet", "Cascadind Support Signals", "Cascading Style Sheet", "Cast System Styling"],
     correctAns: 0
 },
 {
     title: "Javascript enables users to do what?",
-    options: ["Become web-savvy", "Interact with an application", "Become physically fit"],
+    options: ["Become web-savvy", "Become Physically Fit", "Interact with an Application", "Repair Computers"],
     correctAns: 1
 },
 {
     title: "Bootstrap is good for",
-    options: ["Designing applications faster", "Browsing the internet", "Learning how to code"],
+    options: ["Learning How to Code", "Browsing the internet", "Developing Applications Faster", "Building Computers"],
     correctAns: 2
 }
 ]
-
 
 // Question Display
 function showQuestion() {
 questionTitle.textContent = question[0].title
 }
-
+    
 showQuestion();
 
+function nextQuestion() {
+options.forEach(function(element, index) {
+nextBtn.addEventListener("click", function() {
+element.textContent = question[1].options[index];
+nextBtn.addEventListener("click", function() {
+element.textContent = question[2].options[index];
+nextBtn.addEventListener("click", function() {
+element.textContent = question[3].options[index]
+                        })
+                    })
+                })
+            })
+
+
+nextBtn.addEventListener("click", function() {
+questionTitle.textContent = question[1].title;
+nextBtn.addEventListener("click", function() {
+questionTitle.textContent = question[2].title;
+nextBtn.addEventListener("click", function() {
+questionTitle.textContent = question[3].title;
+                }
+            )
+        }
+        )
+    }
+)
+}
+
+nextQuestion();
+
+
+
+
 // Options Display
+// for (var i=0; i < question.length; i++) 
+
 options.forEach(function(element, index) {
 element.textContent = question[0].options[index];
 element.addEventListener("click", function() {
-if (question.correctAns == index) {
-    element.textContent = "Correct!"
+if (question[0].correctAns == index) {
+    element.textContent = "Correct!";
+    ++score;   
+
 } 
 else {
-    element.textContent = "Wrong!"
+    element.textContent = "Wrong!";
 }
 
 })
 });
 
 
-// function theTimer() {
-//     var timeLeft = 10;
-//     var timeInterval = setInterval(function() {
-// if (timeLeft > -1) {
-// timer.textContent = " Time left: " + timeLeft;
-// timeLeft--;
- 
-//     }
-//         },
 
-//             1000);
-// }
+
+function theTimer() {
+    var timeLeft = 60;
+    var timeInterval = setInterval(function() {
+if (timeLeft > -1) {
+timer.textContent = " Time left: " + timeLeft;
+timeLeft--;
+ 
+    }
+        },
+
+            1000);
+}
 
 // theTimer()
 
-
-
-
-
-
-
-// If answer clicked is incorrect reduce time.
-
-// Add four more questions.
-
-// Game ends when timer stops.
-
-// User writes their initials.
 
 
